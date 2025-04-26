6d48f96b1802d43112d82210742f2b6de91d8828
Igor Kulebyakin <igor@Igors-MBP-2.lan>
2025-04-25 18:07:24 -0700

    Added connection string diagnostic message. WIP.

--- app/config/database.py
+++ app/config/database.py
@@ -8,6 +8,8 @@ def connect_db(app: PatchedFastAPI):
     print("Connecting to database...")
     # Set the Stable API version when creating a new client
 
+    print("Connection string:", settings.CONNECTION_STRING)
+
     client = AsyncIOMotorClient(
         settings.CONNECTION_STRING,
         connectTimeoutMS=1000,
